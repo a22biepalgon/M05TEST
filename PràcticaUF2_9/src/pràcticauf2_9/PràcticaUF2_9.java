62fbe6e81a979de544783df67909fa3183a65d24
a22biepalgon <121022409+a22biepalgon@users.noreply.github.com>
2023-01-09 09:15:47 +0100

    Add files via upload

---
+++ PràcticaUF2_9/src/pràcticauf2_9/PràcticaUF2_9.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pràcticauf2_9;
+
+/**
+ *
+ * @author ausias
+ */
+public class PràcticaUF2_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
