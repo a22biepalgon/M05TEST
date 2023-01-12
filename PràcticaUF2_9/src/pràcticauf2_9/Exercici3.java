9c4660bd50eda3722d942648a7108ab8ee0defea
palom <palom@LAPTOP-TCU3RVNO>
2023-01-12 23:33:30 +0100

    Acabat

--- PràcticaUF2_9/src/pràcticauf2_9/Exercici3.java
+++ PràcticaUF2_9/src/pràcticauf2_9/Exercici3.java
@@ -23,7 +23,7 @@ public class Exercici3 {
     public static int restant = 0;
 
     /**
-     * Aquesta funció serveix per a multiplicar dos numeros sense utilitzar la divisió ni la suma ni modul
+     * Aquesta funció serveix per a multiplicar dos numeros positius sense utilitzar la divisió ni la suma ni modul
      * @param a un valor integer
      * @param b un valor integer
      * @return retorna el resultat de a * b
@@ -31,17 +31,19 @@ public class Exercici3 {
     private static int Multiplicar(int a, int b) {
         //Creem la varible resultat
         int resultat = 0;
-        //Si b no és 2
+        //Si b no és 2 que torni a cridar a la funció restant 1 a b, i que sumi el restant + a
         if (b > 2) {
             restant = restant + a;
             resultat = Multiplicar(a, b - 1);
         }
 
+        //Si b == 2, que calculi el resultat de la multiplicació, i així després ho retornarà
         if (b == 2) {
             resultat = (a << 1) + restant;
         }else if(b == 1){
             resultat = a;
         }
+        //Retornem el resultat
         return resultat;
     }
 }
